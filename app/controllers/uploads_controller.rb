83f7d42e8e5c1734734023ff486fcd8efda3370f
xiaoqingnian <golovesx@gmail.com>
2020-12-10 00:24:58 +0800

    uploads

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,6 +1,36 @@
 class UploadsController < ApplicationController
   def create
     @upload = Upload.create!(params.permit(:file))
-    render json: { ret: 1,  data: { ent: Rails.application.routes.url_helpers.rails_blob_path(@upload.file, only_path: true) } }
+    render json: { ret: 1,  data: { ent: @upload.attached_file_url } }
   end
+
+
+  # GET /upload/:id
+  # GET /upload/:id?s=large
+  def show
+    if params[:s]
+      # generate thumb
+      variation_key = Blob.variation(params[:s])
+      send_file_by_disk_key @blob.representation(variation_key).processed.key, content_type: @blob.content_type
+    else
+      # return raw file
+      send_file_by_disk_key @blob.key, content_type: @blob.content_type, disposition: params[:disposition], filename: @blob.filename
+    end
+  end
+
+  private
+
+    def send_file_by_disk_key(key, content_type:, disposition: :inline, filename: nil)
+      opts = { type: content_type, disposition: disposition, filename: filename }
+      send_file Blob.path_for(key), opts
+    end
+
+    def set_blob
+      @blob = ActiveStorage::Blob.find_by(key: params[:id])
+      head :not_found if @blob.blank?
+    end
+
+    def require_disk_service!
+      head :not_found unless Blob.disk_service?
+    end
 end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,4 +1,11 @@
 class Upload < ApplicationRecord
   # 用于存储图片
   has_one_attached :file
+
+
+  def attached_file_url(style: nil)
+    return "" unless self.file.attached?
+    return Rails.application.routes.url_helpers.upload_path(self.file.blob.key) if style.blank?
+    Rails.application.routes.url_helpers.upload_path(self.file.blob.key, s: style)
+  end
 end
