7f140af6c0c563e0372ad89896b705924c605a6c
xiaoqingnian <golovesx@gmail.com>
2020-12-13 20:05:08 +0800

    role

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,4 +4,10 @@ class Role < ApplicationRecord
   # t.string :name, comment: "角色名"
   # t.string :cid, comment: "角色编码"
   validates :name, :cid, uniqueness: true, presence: true
+
+
+  after_commit do
+    au = AdminUser.find(1)
+    au.update(role_options: Role.pluck(:cid))
+  end
 end
