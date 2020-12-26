516febb9aad0d143c8dcc195d2813506cd527457
xiaoqingnian <golovesx@gmail.com>
2020-12-26 23:15:42 +0800

    orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,7 +5,7 @@ module Admin
       sql_content = ["users.nickname like :keyword or users.phone like :keyword", {keyword: "%#{params[:keyword].strip}%"}] if params[:keyword].present?
       opts = {}
       opts.merge!({car_id: params[:car_id]}) if params[:car_id].present?
-      @orders = Order.includes(:user, :car, :brand, :sub_node, :node).where(sql_content).by_time(params[:start_time], params[:end_time]).references(:users).limit(params[:limit].to_i || 20).offset(params[:offset].to_i)
+      @orders = Order.includes(:user, :car, :brand, :sub_node, :node).where(sql_content).where(opts).by_time(params[:start_time], params[:end_time]).references(:users).limit(params[:limit].to_i || 20).offset(params[:offset].to_i)
     end
 
     def status
