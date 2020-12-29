92eef5a04192e3e7856152a9aab803b856b67dda
xiaoqingnian <golovesx@gmail.com>
2020-12-29 22:43:33 +0800

    orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,7 +8,9 @@ module Admin
       if params[:limit].blank?
         params[:limit] = 20
       end
-      @orders = Order.includes(:user, :car, :brand, :sub_node, :node).where(sql_content).where(opts).by_time(params[:start_time], params[:end_time]).references(:users).limit(params[:limit].to_i || 20).offset(params[:offset].to_i)
+      opts = {}
+      opts.merge!({status: params[:status]}) if params[:status].present?
+      @orders = Order.includes(:user, :car, :brand, :sub_node, :node).where(sql_content).where(opts).by_time(params[:start_time], params[:end_time]).where(opts).references(:users).limit(params[:limit].to_i || 20).offset(params[:offset].to_i)
     end
 
     def status
